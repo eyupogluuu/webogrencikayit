61fd516b9a616e82737c784f1cccc92a6645357a
casper <casper@DESKTOP-2PU1K7L>
2022-07-31 15:53:27 +0300

    Add project files.

---
+++ webogrencikayit/ogrencikayit.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webogrencikayit
+{
+    public partial class ogrencikayit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.ogrenciTableAdapter dt = new DataSet1TableAdapters.ogrenciTableAdapter();
+            dt.ogrenciekle(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text);
+            TextBox1.Text = " ";
+            TextBox2.Text = " ";
+            TextBox3.Text = " ";
+            TextBox4.Text = " ";
+        }
+    }
+}
